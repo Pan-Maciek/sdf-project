4e894a7ed63212f14d5d855d0dff7f4602e6574c
Pan-Maciek <koziejka.com@gmail.com>
2020-12-27 20:45:44 +0100

    kdnode

--- sdf-lib/kdtree.cpp
+++ sdf-lib/kdtree.cpp
@@ -1,2 +1,24 @@
 #include "pch.h"
 #include "kdtree.h"
+
+namespace sdf {
+
+void kd_node::init_leaf(int *pnums, int pcount, std::vector<int> &pindices) {
+	flags = kd_leaf | (pcount << 2);
+	switch (pcount) {
+		case 0: split = 0; break;
+		case 1: split = glm::intBitsToFloat(pnums[0]); break;
+		default:
+			split = glm::intBitsToFloat(pindices.size());
+			for (int i = 0; i < pcount; ++i)
+				pindices.push_back(pnums[i]);
+	}
+}
+
+void kd_node::init_interior(int axis, int above_child, float split_value) {
+	flags = axis | (above_child << 2);
+	split = split;
+}
+
+}
+
--- sdf-lib/sdf/kdtree.h
+++ sdf-lib/sdf/kdtree.h
@@ -1,1 +1,24 @@
 #pragma once
+#include "pch.h"
+
+namespace sdf {
+
+struct kd_node {
+	float split;
+	int flags;
+
+	void init_leaf(int *pnums, int pcount, std::vector<int> &pindices);
+	void init_interior(int axis, int above_child, float split);
+};
+
+#define kd_leaf 3
+#define kd_isleaf(node) ((node.flags & 3) == 3)
+
+#define kd_split(node) node.split
+#define kd_split_axis(node) (node.flags & 3)
+#define kd_above_child(node) (node.flags >> 2)
+
+#define kd_pcount(node) (node.flags >> 2)
+#define kd_poffset(node) glm::floatBitsToInt(node.split)
+
+}
